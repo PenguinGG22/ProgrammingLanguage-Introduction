a32a881512ba185807e9aaeca539a7eb8850acdb
펭귄꽝꽝 <pggg2222@gmail.com>
2025-09-13 11:07:51 +0000

    Chapter 02

--- Chapter02.c
+++ Chapter02.c
@@ -1,37 +1,39 @@
 // 02-1
 #include <stdio.h>
 
-int main() {
+int main(void)
+{
     printf("김동건");
-
-	return 0;
+    return 0;
 }
 
 #include <stdio.h>
 
-int main() {
-	printf("김동건 \n");
+int main(void)
+{
+    printf("김동건 \n");
     printf("010-2335-3884");
-
-	return 0;
+    return 0;
 }
 
 // 02-2
 #include <stdio.h>
 
-int main() {
-	printf("제 이름은 김동건입니다. \n");
-	printf("제 나이는 %d살이고요. \n", 20);
-	printf("제가 사는 곳의 번지수는 %d-%d입니다.", 123, 456);
+int main(void)
+{
+    printf("제 이름은 김동건입니다. \n");
+    printf("제 나이는 %d살이고요. \n", 20);
+    printf("제가 사는 곳의 번지수는 %d - %d입니다.", 123, 456);
 
 	return 0;
 }
 
 #include <stdio.h>
 
-int main() {
-	printf("%dx%d=%d \n", 4, 5, 20);
-	printf("%dx%d=%d \n", 7, 9, 63);
+int main(void)
+{
+    printf("%d x %d = %d \n", 4, 5, 20);
+    printf("%d x %d = %d \n", 7, 9, 63);
 
 	return 0;
 }
