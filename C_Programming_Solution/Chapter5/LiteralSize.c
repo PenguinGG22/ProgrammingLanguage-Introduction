3da7b27161d5f5f485989bc10b792d65c2637159
펭귄꽝꽝 <pggg2222@gmail.com>
2025-10-12 14:40:48 +0000

    Update : contents

---
+++ C_Programming_Solution/Chapter5/LiteralSize.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("literal int size : %d \n", sizeof(7));
+    printf("literal int size : %d \n", sizeof(7.14));
+    printf("literal int size : %d \n", sizeof('A'));
+
+    return 0;
+}
