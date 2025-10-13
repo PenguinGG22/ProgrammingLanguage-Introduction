5cd7c6ba8cbd8f48545eacd92ea6da0c63bbec7d
펭귄꽝꽝 <pggg2222@gmail.com>
2025-10-13 12:51:28 +0000

    Update : contents

---
+++ C_Programming/Chapter9/Solution09-2_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int AddToTotal(int num){
+    static int total;
+    total += num;
+    return total;
+}
+
+int main(){
+    int num;
+    for(int i = 0; i < 3; i++){
+        printf("입력%d : ", i + 1);
+        scanf("%d", &num);
+        printf("누적 : %d\n", AddToTotal(num));
+    }
+    return 0;
+}
